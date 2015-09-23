01b05d6944de7a058d768674e3b057a2cce3af45
Alan Rafael R. Batista <alanrrb@gmail.com>
2015-09-23 00:34:58 -0300

    Remove thin and add author

--- Gemfile
+++ Gemfile
@@ -1,8 +1,6 @@
 source 'https://rubygems.org'
 
 gem 'sinatra'
-gem 'thin'
-gem 'sqlite3'
 
 group :development, :test do
   gem 'rack-test'
--- opinionsapp.rb
+++ opinionsapp.rb
@@ -1,36 +1,41 @@
 require 'sinatra'
-require 'sqlite3'
 
-  post '/opinions' do
-    insert params[:opinion]
-    opinions = retrieve_all
-    erb :index, locals: { opinions: opinions }
-  end
+SEPARATOR = "•••"
 
-  get '/' do
-    opinions = retrieve_all
-    erb :index, locals: { opinions: opinions }
-  end
+post '/opinions' do
+  insert params
+  opinions = retrieve_all # cada opinion é um hash com opinion e author
+  erb :index, locals: { opinions: opinions }
+end
 
-  configure do
-    $filename = "#{ENV['RACK_ENV']}.txt"
-    `touch #{$filename}`
-  end
+get '/' do
+  opinions = retrieve_all # cada opinion é um hash com opinion e author
+  erb :index, locals: { opinions: opinions }
+end
 
-  private
+configure do
+  $filename = "#{ENV['RACK_ENV'] || "opinions_db"}.txt"
+  `touch #{$filename}`
+end
 
-  def db
-    $db
-  end
+private
 
-  def retrieve_all
-    file = File.open($filename, "r+")
-    data = file.readlines("\n")
-    file.close
-    data
+def db
+  $db
+end
+
+def retrieve_all
+  file = File.open($filename, "r+")
+  data = file.readlines("\n")
+  file.close
+  data.map do |line|
+    opinion = line.split(SEPARATOR)
+    { opinion: opinion[0], author: opinion[1] }
   end
-  def insert(params)
-    File.open($filename, "a+") do |f|
-      f << params.concat("\n")
-    end
+end
+
+def insert(params)
+  File.open($filename, "a+") do |f|
+    f << params[:opinion] << SEPARATOR << params[:author] << "\n"
   end
+end
